d76629180bb2ac403ce9b8591d2d69390b9563ae
nwy98 <97599088+norunaru@users.noreply.github.com>
2025-07-01 16:04:37 +0900

    [Silver I] Title: 기타리스트, Time: 156 ms, Memory: 10036 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.js
+++ 백준/Silver/1495. 기타리스트/기타리스트.js
@@ -1,31 +1,48 @@
+/*
+기타리스트
+
+N개 곡 연주
+곡 시작하기 전 볼륨 변경
+
+각 곡이 시작하기 전 바꿀 수 있는 볼륨 리스트 V
+V[i] = i번째 곡 연주하기 전 바꿀 수 있는 볼륨
+항상 적힌 차이로만 볼륨 변경 가능
+-> 현재 P면 P+V[i] / P-V[i]
+
+0보다 작거나 M보다 큰 값으로 변경 불가
+
+dp
+*/
+
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-let [N, S, M] = input.shift().split(" ").map(Number);
-// console.log(N, S, M);
+let [N, S, M] = input[0].trim().split(" ").map(Number);
+let vol = input[1].trim().split(" ").map(Number);
 
-let volumes = input.shift().split(" ").map(Number);
-let ans = -1;
 let dp = Array.from({ length: N + 1 }, () => new Array(M + 1).fill(0));
-
 dp[0][S] = 1;
+
+// console.table(dp);
+
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < M + 1; j++) {
     if (dp[i][j] == 1) {
-      let a = j - volumes[i]; //줄인 볼륨
-      let b = j + volumes[i];
-      //   console.log(a, b);
+      let left = j - vol[i];
+      let right = j + vol[i];
+      //   console.log("cur:", j, "vol:", vol[i]);
+      //   console.log("left:", left, "right:", right);
 
-      if (a >= 0) dp[i + 1][a] = 1;
-      if (b <= M) dp[i + 1][b] = 1;
+      if (left >= 0) dp[i + 1][left] = 1;
+      if (right <= M) dp[i + 1][right] = 1;
     }
   }
 }
-// console.table(dp);
 
-for (let i = 0; i < M + 1; i++) {
-  if (dp[N][i] == 1) ans = i;
+let ans = -1;
+for (let j = 0; j <= M; j++) {
+  if (dp[N][j] == 1) ans = j;
 }
 
 console.log(ans);

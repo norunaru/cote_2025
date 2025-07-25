55aca7fbba18e6d4139a5b60144b014c3927977f
nwy98 <97599088+norunaru@users.noreply.github.com>
2025-07-25 13:18:58 +0900

    [Silver II] Title: A → B, Time: 96 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.js
@@ -0,0 +1,38 @@
+/*
+a->b
+
+정수 a를 b로 바꾸려고 한다
+2곱하기
+1을 수의 가장 오른쪽에 추가
+
+[프로세스]
+dp
+
+*2
+*10+1
+메모리초과나네
+
+역방향 그리디
+
+*/
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let [A, B] = input[0].split(" ").map(Number);
+let count = 1;
+
+while (B > A) {
+  if (B % 2 === 0) {
+    B = B / 2;
+  } else if (B % 10 === 1) {
+    B = Math.floor(B / 10);
+  } else {
+    console.log(-1);
+    return;
+  }
+  count++;
+}
+
+console.log(B === A ? count : -1);

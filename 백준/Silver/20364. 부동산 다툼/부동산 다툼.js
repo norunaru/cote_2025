526802bd41d82277f25402443a85b67934f9d1c0
nwy98 <97599088+norunaru@users.noreply.github.com>
2025-07-29 14:04:35 +0900

    [Silver I] Title: 부동산 다툼, Time: 388 ms, Memory: 65216 KB -BaekjoonHub

--- 백준/Silver/20364. 부동산 다툼/부동산 다툼.js
+++ 백준/Silver/20364. 부동산 다툼/부동산 다툼.js
@@ -2,55 +2,59 @@
 부동산 다툼
 
 이진 트리 모양 땅
-루트 땅 = 1
-왼자식 = 2*k, 오른자식 = 2*k+1
+루트가 1
+땅 번호가 K면 왼자식은 2K, 오른자식은 2K+1
 
-원하는 땅까지 가는 길에 점유된 땅이 있으면 못감
+오리 한 줄로 대기, 1번에 위치
+서있는 순서대로 원하는 땅 가짐
 
-<프로세스>
-루트부터 탐색 시작해야함
-n이 짝수 = 왼자식, 홀수 = 오른자식
+가는 길에 점유된 땅 있으면 처음 마주치는 땅의 번호
 
-n이 1 될때까지 거치는 길 저장
-if(n%2==0) n=n/2, temp.push(n/2);
-
-
-isVisited배열 생성(N+1), false
-
+[프로세스]
+원하는 역순부터 역순으로 1까지 게산
+홀수면 -1하고 /2
 */
+
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-let [N, Q] = input[0].split(" ").map(Number);
-let end = input.slice(1, Q + 1).map(Number);
-let answer = new Array(Q).fill(0);
-let visited = new Set(); // 점유된 땅 저장
-let nodes = []; // 경로 저장용 스택
+let [N, Q] = input.shift().trim().split(" ").map(Number);
+let isVisited = new Array(N + 1).fill(false);
+let ans = [];
 
 for (let i = 0; i < Q; i++) {
-  let endNum = end[i];
+  let want = Number(input[i].trim());
+  let curNo = want;
+  let temp = [];
+  let flag = true;
 
-  // 지나가는 노드들을 저장
-  while (endNum > 0) {
-    nodes.push(endNum);
-    if (endNum % 2 === 1) endNum--; // 홀수라면 -1 해서 부모 찾기
-    endNum = Math.floor(endNum / 2);
+  while (curNo >= 1) {
+    temp.push(curNo);
+    if (curNo == 1) {
+      curNo -= 1;
+    } else if (curNo % 2 == 1) {
+      curNo = (curNo - 1) / 2;
+    } else {
+      curNo = curNo / 2;
+    }
   }
 
-  let isReach = true;
-  while (nodes.length > 0) {
-    let node = nodes.pop();
+  temp.reverse();
 
-    if (visited.has(node)) {
-      answer[i] = node; // 처음 마주치는 점유된 땅
-      isReach = false;
+  for (let j = 0; j < temp.length; j++) {
+    let idx = temp[j];
+    if (isVisited[idx] == true) {
+      flag = false;
+      ans.push(idx);
       break;
     }
   }
 
-  if (isReach) visited.add(end[i]); // 원하는 땅을 차지할 수 있다면 점유 처리
-  nodes.length = 0; // 스택 초기화
+  if (flag) {
+    isVisited[temp[temp.length - 1]] = true;
+    ans.push(0);
+  }
 }
 
-console.log(answer.join("\n")); // 출력 최적화
+console.log(ans.join("\n"));

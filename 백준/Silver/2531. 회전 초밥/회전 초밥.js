659ee7a688057fffe72d5d967e25692cad48ce52
nwy98 <97599088+norunaru@users.noreply.github.com>
2025-09-26 13:23:04 +0900

    [Silver I] Title: 회전 초밥, Time: 3620 ms, Memory: 27892 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.js
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.js
@@ -1,18 +1,4 @@
-/*
-회전초밥
 
-벨트 위 같은 종류 초밥 존재 가능
-
-1. 벨트 임의 위치부터 k개 연속 먹으면 할인 가격
-2. 초밥 종류 쿠폰 발행, 1번 행사 참가시 이 쿠폰에 적힌 초밥 무료 제공
-    벨트 위에 없으면 만들어서 줌
-
-가능한 한 다양한 종류의 초밥을 먹으려고 한다
-
-[프로세스]
-O(N)으로 처리
-배열 검사하면서 set으로 만들고 개수 처리
-*/
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
@@ -26,20 +12,21 @@ for (let i = 0; i < N; i++) {
 }
 
 for (let i = 0; i < k; i++) {
-  arr.push(arr[i]);
+  arr.push(Number(input[i].trim()));
 }
 
 let ans = 0;
-let temp;
 
 for (let i = 0; i < N; i++) {
-  temp = arr.slice(i, i + k);
-  let set = new Set();
-  for (v of temp) {
-    set.add(v);
+  let s = new Set();
+  for (let j = 0; j < k; j++) {
+    s.add(arr[i + j]);
   }
-  let count = set.size;
-  if (!set.has(c)) count++;
+
+  let count = s.size;
+
+  if (!s.has(c)) count++;
+
   ans = Math.max(ans, count);
 }
 
